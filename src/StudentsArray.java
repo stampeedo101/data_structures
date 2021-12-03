4f13b73c2c158bff3b1427214eab030bfc0e6b13
Rafael Martins <rafael.dev101@gmail.com>
2021-12-03 19:55:29 -0300

    insert Student.java model for a student StudentsArray.java class to study simple array behavior.

---
+++ src/StudentsArray.java
@@ -0,0 +1,21 @@
+public class StudentsArray {
+
+    private Student[] students = new Student[200];
+
+    /*
+    addElement()
+    addElementPosition()
+    addElementBegin()
+    addElementEnd()
+
+    getElement()
+
+    deleteElement()
+    deleteBegin()
+    deleteEnd()
+
+    getLentgh()
+
+     */
+
+}
